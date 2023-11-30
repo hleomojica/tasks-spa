a4f3d0202a95ddf991d2aabde00502a16c4fe646
Henrry Mojica <hleomojica@gmail.com>
2023-11-30 15:01:35 +1100

    lab: tasks items

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-// import {TASKS} from '../mock-tasks';
 import {Task} from '../Task';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -17,4 +16,12 @@ export class TaskService {
   getTasks(): Observable<Task[]>{
     return this.http.get<Task[]>(this.apiUrl);
   }
+
+  deleteTask(task:Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  // createTask(task:Task):Observable<Task>{
+  // }
 }
