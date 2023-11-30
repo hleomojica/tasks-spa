cb72630797bc85d1d59ab6ec10f3091b9ddd0909
Henrry Mojica <hleomojica@gmail.com>
2023-11-30 14:47:55 +1100

    lab: add services

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
-import {TASKS} from '../mock-tasks';
+// import {TASKS} from '../mock-tasks';
 import {Task} from '../Task';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3000/api/v1/tasks/';
 
-  constructor() { }
+
+  constructor(private http:HttpClient) {}
 
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS); // Turn in to an observable
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
